739eeac3538b2bfd1efa3be52b1dff6ffc9ad9f4
sakura <chou.creme.294@gmail.com>
2023-12-07 20:55:45 +0900

    first commit

---
+++ src/main/java/com/example/StudyPlan/StudyPlanApplication.java
@@ -0,0 +1,13 @@
+package com.example.StudyPlan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyPlanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudyPlanApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/StudyPlan/StudyPlanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.StudyPlan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyPlanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
