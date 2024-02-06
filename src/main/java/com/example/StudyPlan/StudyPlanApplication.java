6c809c7258702830016ea2215ad00c09f25a20cc
sakura <chou.creme.294@gmail.com>
2024-02-06 15:04:32 +0900

    AWSへのデプロイ

--- src/main/java/com/example/StudyPlan/StudyPlanApplication.java
+++ src/main/java/com/example/StudyPlan/StudyPlanApplication.java
@@ -2,12 +2,20 @@ package com.example.StudyPlan;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
 
 @SpringBootApplication
-public class StudyPlanApplication {
+public class StudyPlanApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(StudyPlanApplication.class, args);
+	}
+	
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+	    return application.sources(StudyPlanApplication.class);
 	}
 
 }
